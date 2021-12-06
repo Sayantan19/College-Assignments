11a725c8d2b1d6b6d0523341ac490865b6e16101
Sayantan19 <75779600+Sayantan19@users.noreply.github.com>
2021-12-06 18:36:01 +0530

    Added stack code

---
+++ College 2nd Year/Week 4/Stack implementation.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Stack{
+    int top;
+    int capacity;
+    int * data;
+};
+
+struct Stack * create(int);
+int isFull(struct Stack *);
+int isEmpty(struct Stack *);
+void push(struct Stack *,int);
+int pop(struct Stack * );
+int peek(struct Stack *);
+void display( struct Stack *);
+
+int main()
+{
+    int n;
+    printf("Enter the number of elements in the stack: ");
+    scanf("%d",&n);
+    struct Stack * stack = create(n);
+    for(int i=0;i<n;i++)
+    {
+        int c;
+        scanf("%d",&c);
+        push(stack,c);
+    }
+    printf("The elements of the stack are: ");
+    display(stack);
+}
+
+struct Stack * create(int n)
+{
+    struct Stack *p = (struct Stack *)malloc(sizeof(struct Stack));
+    p->capacity=n;
+    p->top = -1;
+    p->data = (int *)malloc(sizeof(int) * n);
+    return p;
+}
+
+int isFull(struct Stack * stack)
+{
+    if(stack->top == stack->capacity-1)
+        return 1;
+    else
+        return 0;
+}
+
+int isEmpty(struct Stack * s)
+{
+    if(s->top==-1)
+    return 1;
+    else
+    return 0;
+}
+
+void push(struct Stack * s,int d)
+{
+    if(isFull(s))
+    {
+        printf("Overflow!!!!\n");
+        return;
+    }
+    s->data[++s->top] = d;
+    printf("Element pushed\n");
+}
+
+int pop(struct Stack * s)
+{
+    if(isEmpty(s))
+    {
+        printf("Underflow!!!!!\n");
+        return 0;
+    }
+    return s->data[s->top--]; 
+}
+
+int peek(struct Stack * s)
+{
+    if(isEmpty(s))
+        return -1;
+    return s->data[s->top];
+}
+
+void display(struct Stack * s)
+{
+    if(isEmpty(s))
+        return;
+    printf("%d ",peek(s));
+    pop(s);
+    display(s);
+}
+
