535a54da47e86a7bc77519515cd6d15726c06888
Sayantan19 <75779600+Sayantan19@users.noreply.github.com>
2022-01-12 19:08:25 +0530

    completed the Doubly linked list program finally!

--- College 2nd Year/Day 8/Doubly_Linked_List.c
+++ College 2nd Year/Day 8/Doubly_Linked_List.c
@@ -8,36 +8,103 @@ typedef struct node
 	struct node *next;
 } node;
 
-node *create(int);
-node *InsertBeg(node *, int);
-node *InsertMid(node *, int);
-node *InsertMidBefore(node *, int, int);
-node *InsertMidAfter(node *, int, int);
-node *InsertEnd(node *, int);
-node *DelMidBefore(node *,int);
-node *DelMidAfter(node *,int);
-node *DelBeg(node *);
-node *DelEnd(node *);
+node * Insert(node *);
+node * Delete(node *);
+node * create(int);
+node * InsertBeg(node *, int);
+node * InsertMid(node *, int);
+node * InsertMidBefore(node *, int, int);
+node * InsertMidAfter(node *, int, int);
+node * InsertEnd(node *, int);
+node * DelMidBefore(node *,int);
+node * DelMidAfter(node *,int);
+node * DelBeg(node *);
+node * DelEnd(node *);
 void display(node *);
 
 int main()
 {
-	//A few test cases by myself
-	int a;
-	// printf("Enter the element: ");
-	// scanf("%d",&a);
-	node *n = create(56);
-	n = InsertBeg(n,67);
-	n = InsertEnd(n, 23);
-	n = InsertEnd(n, 69);
-	display(n);
-	// n = InsertMidAfter(n, 56, 23);
-	// display(n);
-	n = DelMidBefore(n,23);
-	display(n);
+	int choice,x;
+	node * n;
+	do
+	{	
+		printf("1 for Create\n2 for Insert\n3 for Delete\n4 for Display\n5 for Exit\nEnter your choice: ");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:		printf("Enter the element with wich you want to create the List: ")	;
+						scanf("%d",&x);
+						n = create(x);
+						printf("Node created successfully!\n");
+						break;
+
+			case 2:		printf("Welcome to Insertion Operation!\n");
+						n = Insert(n);
+						break;
+			
+			case 3: 	printf("Welcome to Deletion Operation!\n");
+						n = Delete(n);
+						break;
+
+			case 4:		display(n);
+						break;
+
+			case 5:		return 0;
+						break;
+			
+			default:	printf("Wrong choice!\n");
+		}
+	}while(1);
 	return 0;
 }
 
+node * Insert(node * n)
+{
+	int choice,element,target;
+	printf("Enter the element to be inserted: ");
+	scanf("%d",&element);
+	printf("1 for Insertion in the beginning\n2 for Insertion at the end\n3 for Insertion before target element\n4 for Insertion after target element\nEnter your choice:");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:		n = InsertBeg(n,element);
+					break;
+		case 2:		n = InsertEnd(n,element);
+					break;
+		case 3:		printf("Enter the target element: ");
+					scanf("%d",&target);
+					n = InsertMidBefore(n,target,element);
+					break;
+		case 4:		printf("Enter the target element: ");
+					scanf("%d",&target);
+					n = InsertMidAfter(n,target,element);
+					break;
+	}
+	return n;
+}
+
+node * Delete(node * n)
+{
+	int choice,target;
+	printf("1 for Deletion in the beginning\n2 for Deletion at the end\n3 for Deletion before target element\n4 for Deletion after target element\nEnter your choice:");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:		n = DelBeg(n);
+					break;
+		case 2:		n = DelEnd(n);
+					break;
+		case 3:		printf("Enter the target element: ");
+					scanf("%d",&target);
+					n = DelMidBefore(n,target);
+					break;
+		case 4:		printf("Enter the target element: ");
+					scanf("%d",&target);
+					n = DelMidAfter(n,target);
+					break;
+	}
+	return n;
+}
 node *create(int n)
 {
 	node *s = (node *)malloc(sizeof(node));
@@ -183,7 +250,30 @@ node * DelMidBefore(node * head,int target)
 			// p = p->next;
 		(p->next)->prev = p->prev;
 		(p->prev)->next = p->next;
-		printf("%p",p->prev);
+		// printf("%p",p->prev);
+	}
+	return head;
+}
+
+node * DelMidAfter(node * head,int target)
+{
+	node * p = head;
+	if(!Search(head,target))
+	{
+		printf("Element not found. Cannot delete!\n");
+		return head;
+	}
+	while(p->data != target)
+		p = p->next;
+	if(p->next == NULL)
+		printf("There is no element after this. Cannot delete!\n");
+	else if(p->next->next == NULL)
+		DelEnd(head);
+	else
+	{
+		p = p->next;
+		(p->next)->prev = p->prev;
+		(p->prev)->next = p->next;
 	}
 	return head;
 }
--- College 2nd Year/For myself, based on the class/Linked List/Doubly/Printing stuff.c
+++ College 2nd Year/For myself, based on the class/Linked List/Doubly/Printing stuff.c
@@ -1,108 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 typedef struct Node
 {
-    struct Node *next;
+    struct Node * next;
     int data;
-    struct Node *prev;
-} node;
+    struct Node * prev;
+}node;
 
-node *create(int a)
+node * create(int a)
 {
-    node *n = (node *)malloc(sizeof(node));
+    node * n = (node *)malloc(sizeof(node));
     n->next = NULL;
     n->data = a;
     n->prev = NULL;
     return n;
 }
 
-void display(node *head)
+void display(node * head)
 {
-    node *p = head;
-    if (head == NULL)
+    node * p = head;
+    if(head == NULL)
     {
         printf("The list is empty\n");
         return;
     }
     printf("Prev\t\tData\t\tNext\n");
-    while (p != NULL)
+    while(p!=NULL)
     {
-        printf("%p\t%d\t%p\n", p->prev, p->data, p->next);
+        printf("%p\t%d\t%p\n",p->prev,p->data,p->next);
         p = p->next;
     }
 }
 
-node *InsertEnd(node *head, int a)
+node * InsertEnd(node * head,int a)
 {
-    node *temp = create(a);
-    node *p = head;
-    if (head == NULL)
-        head = temp;
-    else
-    {
-        while (p->next != NULL)
-            p = p->next;
-        p->next = temp;
-        temp->prev = p;
-    }
-    return head;
+	node * temp = create(a);
+	node * p = head;
+	if(head == NULL)
+		head = temp;
+	else
+	{
+		while(p->next!=NULL)
+			p = p->next;
+		p->next = temp;
+		temp->prev = p;
+	}
+	return head;
 }
 
-node *InsertMidBefore(node *head, int target, int a)
-{
-    node *temp = create(a);
-    node *p = head;
-    if (!Search(head, target))
-        printf("Target element not in the list. Cannot add!");
-
-    else if (head->data == target)
-        head = InsertBeg(head, a);
-
-    else
-    {
-        while (p->next->data != target)
-            p = p->next;
-        temp->next = p->next;
-        temp->prev = p;
-        p->next = temp;
-    }
-    return head;
-}
-
-node *InsertMidAfter(node *head, int target, int a)
-{
-    node *temp = create(a);
-    node *p = head;
-    if (!Search(head, target))
-        printf("Target element not in the list. Cannot add!\n");
-    while (p->data != target)
-        p = p->next;
-    if (p->next == NULL)
-        p = InsertEnd(p, a);
-    else
-    {
-        temp->next = p->next;
-        temp->prev = p;
-        p->next = temp;
-    }
-    return head;
-}
 
 int main()
 {
-    node *head;
+    node * head;
     int x;
     printf("Enter the 1 element: ");
-    scanf("%d", &x);
+    scanf("%d",&x);
     head = create(x);
-    for (int i = 1; i < 5; i++)
+    for(int i=1;i<5;i++)
     {
-        printf("Enter the %d element: ", (i + 1));
-        scanf("%d", &x);
-        head = InsertEnd(head, x);
+        printf("Enter the %d element: ",(i+1));
+        scanf("%d",&x);
+        head = InsertEnd(head,x);
     }
-    head = InsertEnd(head, 23,x);
     display(head);
     return 0;
 }
